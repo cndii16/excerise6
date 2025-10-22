f0ecbc74e5149a866a18228ca4ac04458ada6dff
Jamilla Reynolds <jamirose10@gmail.com>
2025-10-22 00:32:14 -0600

    please tlel me this works this time

---
+++ src/Components/Meals/AvailableMeals.jsx
@@ -0,0 +1,81 @@
+//video 4 begin, video 5: 8 min mark when more (mental note in case i bungle something)
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+import { useEffect, useState } from "react";
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch(
+          "https://meal-app-2b0de-default-rtdb.firebaseio.com/meals.json"
+        );
+
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+
+        const responseData = await response.json();
+
+        const loadedMeals = [];
+
+        for (const key in responseData) {
+          loadedMeals.push({
+            id: key,
+            name: responseData[key].name,
+            description: responseData[key].description,
+            price: responseData[key].price,
+          });
+        }
+
+        setMeals(loadedMeals);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setHttpError(error.message);
+      }
+    };
+    fetchMeals();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      id={meal.id}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
---
+++ src/Components/Meals/MealsSummary.jsx
@@ -0,0 +1,13 @@
+import classes from "./MealsSummary.module.css";
+const MealsSummary = () => {
+    return(
+        <section className={classes.summary}>
+            <h2>Delicious food, Delivered to you</h2>
+            <p>Choose your favorite meal from our board selection and get it Delivered</p>
+            <p>yummy food for ya weeeeeee woooooooooooooooo</p>
+        </section>
+    )
+}
+
+
+export default MealsSummary;
